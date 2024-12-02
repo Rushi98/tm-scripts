3850e936d39b24eea8c501edd5e21c0d660cd06a
Rushikesh Jogdand <rushikeshjogdand1@gmail.com>
2024-12-02 09:33:37 +0530

    Create Fm view video 1.0.js

---
+++ Fm view video 1.0.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Fm view video 1.0
+// @namespace    http://tampermonkey.net/
+// @version      2024-01-23
+// @description  try to take over the world!
+// @author       You
+// @match        https://academy.forumias.com/404/*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=forumias.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const p = document.location.pathname.split('/');
+    const id = p[2];
+    const token = p[3];
+
+    document.body.innerHTML='<iframe src="https://forumias.testpress.in/embed/'+id+'/?access_token='+token+'" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope picture-in-picture" allowfullscreen="" crossorigin="anonymous" frameborder="0" style="border: 0px; max-width: 100%; position: absolute; top: 0px; left: 0px; height: 100%; width: 100%;"></iframe>';
+    // Your code here...
+})();
